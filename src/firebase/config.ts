2f56dd599718262635154a0ae6ddcd2447b7ae99
WIMCB <wimcb.solbordynamics@gmail.com>
2026-03-15 18:05:42 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyB-yO5L_DummyRealKey_For_WIMCB",
+  apiKey: "AIzaSyC2KJoqM6Djupmeumg_dmApfkNdN_XdVKQ",
   authDomain: "wimcb-b3270.firebaseapp.com",
   projectId: "wimcb-b3270",
   storageBucket: "wimcb-b3270.firebasestorage.app",
-  messagingSenderId: "765432109876",
-  appId: "1:765432109876:web:abcd1234efgh5678"
+  messagingSenderId: "402341987844",
+  appId: "1:402341987844:web:26aa7e4a7eff005baf1751",
+  measurementId: "G-D9BK1KRJD7"
 };
